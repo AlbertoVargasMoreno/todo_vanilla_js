4fc8c90ffaa9ea961c42e7c1f79fda7c58ab9a27
Alberto Vargas <albertovargas@intelligo.mx>
2024-03-28 13:30:53 -0600

    WIP: use localStorage to make todo persisent

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
+let listState = [];
+const STATE_KEY = "todo-list";
+
+function loadState() {
+	const listState = localStorage.getItem(STATE_KEY);
+	if (listState !== null) {
+		return JSON.parse(listState);
+	}
+	return [];
+}
+
+function saveState(list) {
+	localStorage.setItem(STATE_KEY, JSON.stringify(list));
+}
+
+function initList() {
+	listState = loadState();
+	const tasksList = document.getElementById("list");
+	for (const task of listState) {
+		const li = document.createElement("li");
+		li.innerText = task.text;
+
+		const deleteButton = document.createElement("span");
+		deleteButton.classList.add("deleter");
+		deleteButton.onclick = deleteItem;
+		li.appendChild(deleteButton);
+		
+		li.classList.add("item");
+		if (task.checked) {
+			li.classList.add("checked");
+		}
+
+		tasksList.appendChild(li);
+	}
+	// needs to render the delete button
+}
+
 const addButton = document.getElementById("add-button");
 addButton.addEventListener("click", addItem);
 
@@ -7,6 +44,8 @@ form.addEventListener("submit",
 		e.preventDefault();
 	}
 );
+
+initList();
 
 function addItem() {
 	const ul = document.getElementById("list");
@@ -27,6 +66,12 @@ function addItem() {
 	deleteButton.onclick = deleteItem;
 
 	newItem.appendChild(deleteButton);
+	
+	listState.push({
+		text,
+		checked: false
+	});
+	saveState(listState);
 
 	input.value = "";
 	ul.appendChild(newItem);
